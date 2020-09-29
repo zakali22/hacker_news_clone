6104d83225aff258ca05420bb5a78e6bb7074069
Zakeria Ali <zak.fined@gmail.com>
2020-09-29 20:29:46 +0100

    Add project structure

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index__bundle.js',
+        publicPath: '/'
+    },
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    module: {
+        rules: [
+            {test: /\.(js)$/, use: 'babel-loader'},
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            {test:  /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader']},
+            {test: /\.(png|svg|jpg|gif)$/, use: ['file-loader']}
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './src/assets', to: 'public/assets' }, 
+                // {from: '_redirects'}
+              ]
+        })
+    ],
+    devServer: {
+        historyApiFallback: true
+    }
+}
