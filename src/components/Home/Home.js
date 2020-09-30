794a4ff674233c187d2b0973814674b6992dee52
Zakeria Ali <zak.fined@gmail.com>
2020-09-30 22:42:46 +0100

    Fetch top news on mount and display the results (setting them to the localStorage)

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react'
+import {fetchTopNews} from "../../utils/api"
+import {Link} from "react-router-dom"
 
 class Home extends Component {
+    state = {
+        posts: null
+    }
+
+    componentWillMount(){
+        this.setState({
+            posts: JSON.parse(
+                window.localStorage.getItem("posts")
+              ) || null
+        })
+    }
+
+    componentDidMount(){
+        console.log(this.props)
+        fetchTopNews().then((res) => {
+            console.log(res)
+            this.setState({
+                posts: res
+            }, () => {
+                window.localStorage.setItem("posts", JSON.stringify(this.state.posts));
+            })
+        })
+    }
+
     render() {
+        const {posts} = this.state;
+        const {match} = this.props
+        if(!posts) return <p>Loading</p>
         return (
-            <div>
-                Home
+            <div className="home">
+                <div className="container">
+                    <div className="post-listing">
+                        {posts.map(post => {
+                            return (
+                                <div key={post.id} className="post-listing__post">
+                                    <a href={post.url} className="post-listing__post-title">{post.title}</a>
+                                    <p>by <Link to={`/user?id=${post.by}`}>{post.by}</Link> 9/30/2020, 9:08 PM  with {post.descendants} comments</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
         )
     }
