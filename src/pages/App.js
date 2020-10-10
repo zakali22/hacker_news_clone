451a309d1088398dc450294e755d36f71c0ae7ae
Zakeria Ali <zak.fined@gmail.com>
2020-10-10 23:57:05 +0100

    Add code-splitting and fix memory leakage

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,14 @@
-import React, {Component} from "react"
+import React, {Component, Suspense, lazy} from "react"
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import Nav from "../components/Nav/Nav"
 import {ThemeProvider} from "../context/Theme"
+import Loading from "../components/Loading/Loading"
 
-/** Implement Code splitting - TODO */
-import Home from "./Home/Home.js"
-import User from "./User/User.js"
-import Post from "./Post/Post.js"
-import New from "./New/New.js"
+const Home = lazy(() => import("./Home/Home.js"))
+const User = lazy(() => import("./User/User.js"))
+const Post = lazy(() => import("./Post/Post.js"))
+const New = lazy(() => import("./New/New.js"))
+
 
 class App extends Component {
     state = {
@@ -26,15 +27,17 @@ class App extends Component {
     render(){
         return (
             <ThemeProvider value={this.state}>
-                <Router>
-                    <Nav />
-                    <Switch>
-                        <Route exact path={"/"} component={Home} />
-                        <Route path={"/user"} component={User} />
-                        <Route path={"/post"} component={Post} />
-                        <Route path={"/new"} component={New} />
-                    </Switch>
-                </Router>
+                <Suspense fallback={<Loading />}>
+                    <Router>
+                        <Nav />
+                        <Switch>
+                            <Route exact path={"/"} component={Home} />
+                            <Route path={"/user"} component={User} />
+                            <Route path={"/post"} component={Post} />
+                            <Route path={"/new"} component={New} />
+                        </Switch>
+                    </Router>
+                </Suspense>
             </ThemeProvider>
         )
     }
